6c21c3a0bf61910fc5116cfcde68dab1350235b0
SlazZzor <slazzzor@gmail.com>
2024-08-31 15:23:53 +0200

    Initial Commit

---
+++ PolymarketWidget/PolymarketWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolymarketWidgetApp.swift
+//  PolymarketWidget
+//
+//  Created by Slavik Rozputnii on 31.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct PolymarketWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
